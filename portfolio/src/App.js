9a7823badd3b42a31ad5e81cf844d8a67c0490a1
Harshpreetkaur98 <harshpreet.149kaur@gmail.com>
2024-06-22 15:48:52 +0100

    Banner Page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { MyNavBar } from './components/NavBar';
+import { Banner } from './components/Banner'
 import { Nav } from 'react-bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">  
         <MyNavBar />
+        <Banner />
     </div>
   );
 }
